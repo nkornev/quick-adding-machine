ce6b17ec427ce3092686a55f9a224e989d9ae989
Nick Kornev <nick@themarksys.com>
2015-07-30 17:02:05 -0700

    working on main form

---
+++ com/kornevs/nick/adding_machine/CalcForm.java
@@ -0,0 +1,13 @@
+package com.kornevs.nick.adding_machine;
+
+import javax.swing.*;
+
+/**
+ * Created by nick on 7/30/15.
+ */
+public class CalcForm {
+    private JPanel panMain;
+    private JLabel lbNumInput;
+    private JTextField txtNumInput;
+    private JTable table1;
+}
